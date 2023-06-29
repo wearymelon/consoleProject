d6354baa2a127e49b6c68042fab99524d5da8ec1
wearymelon <maxthecouchpotato@gmail.com>
2023-06-28 17:36:15 -0700

    finished shopping list, began bank

---
+++ Bank/Bank/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Bank
+{
+    public class Class1
+    {
+        //make a bank class
+        //bank has an array of accounts (account is another class)
+        //account has general info (money, username, password)
+
+        //make menu to login and deposit/withdraw/transfer money
+
+        //Bonus: make the account array work the same was as shopping list (expand/contract)
+    }
+}
