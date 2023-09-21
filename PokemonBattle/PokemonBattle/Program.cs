56122305207bc57e0e20148459db81e4716437bb
Daniel.Makhmud <Daniel.Makhmud@GMR-118-2-2>
2023-09-20 17:33:30 -0700

    close

--- PokemonBattle/PokemonBattle/Program.cs
+++ PokemonBattle/PokemonBattle/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace PokemonBattle
+﻿using System.Runtime.CompilerServices;
+
+namespace PokemonBattle
 {
     internal class Program
     {
@@ -71,7 +73,7 @@
 
                                 if (opponent.GetHit(pokemons[MyPokemon].AttackDamage))
                                 {
-                                    Console.WriteLine($"{opponent.Print()} has taken damage! They are now at {opponent.Health} health.");   
+                                    Console.WriteLine($"{opponent.Print()} has taken damage! They are now at {opponent.Health} health. \n");   
                                 }
 
                                 //Don't use this line, this was just Nikita's experiment:
@@ -80,13 +82,42 @@
 
                                 if (pokemons[MyPokemon].GetHit(opponent.AttackDamage))
                                 {
-                                    Console.WriteLine($"You have taken damage! You are now at {pokemons[MyPokemon].Health} health.");
+                                    Console.WriteLine($"Opponent is making a move... You have taken damage! You are now at {pokemons[MyPokemon].Health} health. \n");
                                 }
                             }
 
                             else if (attackChoice == "special")
                             {
-                                pokemons[MyPokemon].SpecialMove(opponent);
+
+                                if (pokemons[MyPokemon].SpecialCount > 0)
+                                {
+
+                                    pokemons[MyPokemon].SpecialMove(opponent);
+
+
+
+                                    Console.WriteLine($"You have performed your special move! You are at {pokemons[MyPokemon].Health} health and your opponent is at {opponent.Health} health. \n");
+
+                                    Console.WriteLine($"You have {pokemons[MyPokemon].SpecialCount} special uses left before cooldown is required. \n");
+
+                                }
+
+                                else
+                                {
+                                    Console.WriteLine("You do not have any special uses left! Cooldown is now required.  \n");
+                                }
+
+                                if (opponent.SpecialCount > 0)
+                                {
+                                    opponent.SpecialMove(pokemons[MyPokemon]);
+
+
+                                    Console.WriteLine($"The opponent has used their special move!  You are at {pokemons[MyPokemon].Health} health and your opponent is at {opponent.Health} health. \n");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Your opponent has tried to use their specal move on you but they are out of uses and need to let it cooldown. Looks like you get to catch a break! \n");
+                                }
                             }
 
                             if (pokemons[MyPokemon].Health <= 0 || opponent.Health <= 0)
